d5de4480ccbf63dbc48c17bc4fbd75f77e09d540
Martu <100697743+maralmi@users.noreply.github.com>
2023-07-12 22:10:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+        }
+    }
+}
